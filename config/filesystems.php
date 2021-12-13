635396aa919549a4b062095f8d47b3c4bff64dfb
Muhamad iqbal Ilham Rahayu <70595626+x-bal@users.noreply.github.com>
2021-12-13 13:40:02 +0700

    storage

--- config/filesystems.php
+++ config/filesystems.php
@@ -37,11 +37,11 @@ return [
 
         'public' => [
             'driver' => 'local',
-            'root' => '/home/mxbalmeu/api.portfolio/storage/app/public'),
+            'root' => storage_path('app/public'),
             'url' => env('APP_URL') . '/storage',
             'visibility' => 'public',
         ],
-
+        
         's3' => [
             'driver' => 's3',
             'key' => env('AWS_ACCESS_KEY_ID'),
